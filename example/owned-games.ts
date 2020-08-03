4dc5133b884cdd24bdb6284e167c661a00d42fe1
WMXPY <wm@mengw.io>
2020-08-02 20:33:31 -0500

    fix example

--- example/owned-games.ts
+++ example/owned-games.ts
@@ -4,11 +4,14 @@
  * @description Owned Games
  */
 
+import { axiosDriver } from "@barktler/driver-axios";
 import { OwnedGamesDetail, SteamAPI } from "../src";
 
 (async () => {
 
     const steamApi: SteamAPI = SteamAPI.create('steam api key');
+    steamApi.useDriver(axiosDriver);
+
     const detail: OwnedGamesDetail = await steamApi.getOwnedGames('steam id');
 
     console.log(detail);
